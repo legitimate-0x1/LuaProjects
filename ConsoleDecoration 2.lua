7f24b3facab8ca00347f6a0b9b09aa1dc8ad364f
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-05-28 20:09:43 +0300

    Create ConsoleDecoration 2.lua

---
+++ ConsoleDecoration 2.lua
@@ -0,0 +1,12 @@
+-- Made by Sovf!
+
+local Char = "#"
+
+for Index = 1, 16 do
+    print(string.rep(Char, Index))
+end
+
+for Index = 1, 16 do
+    local Number = 16 - Index
+    print(string.rep(" ", Index) .. string.rep(Char, Number))
+end
